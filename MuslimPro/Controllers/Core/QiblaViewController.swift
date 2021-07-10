5b9c932536f14b48352d1dfba4097284ad7b9ad0
Sa3doola <saadsherif02@gmail.com>
2021-07-11 00:45:39 +0200

    First Init and Setup Project

---
+++ MuslimPro/Controllers/Core/QiblaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QiblaViewController.swift
+//  MuslimPro
+//
+//  Created by Saad Sherif on 7/11/21.
+//
+
+import UIKit
+
+class QiblaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MuslimPro/Controllers/Core/TimesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimesViewController.swift
+//  MuslimPro
+//
+//  Created by Saad Sherif on 7/11/21.
+//
+
+import UIKit
+
+class TimesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
